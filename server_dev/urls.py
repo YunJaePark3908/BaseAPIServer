17ab922917541406a3c2d75b428614ce97152a16
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2021-07-07 19:53:22 +0900

    First Init

---
+++ server_dev/urls.py
@@ -0,0 +1,8 @@
+# server_dev/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', include('login.urls')),
+]
